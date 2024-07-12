4b96ae3186e885512c8abaa246d27d568204af9e
unknown <daniel.deaconescu23@gmail.com>
2024-07-12 16:18:39 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function runTypingEffect() {
+  const text = "I am Daniel Deaconescu";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
